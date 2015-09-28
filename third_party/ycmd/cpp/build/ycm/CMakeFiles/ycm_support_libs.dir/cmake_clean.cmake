f0e402dd4d0c0be888ff05586c0164e4ebfceb44
snakeleon <snakeleon@sina.com>
2015-09-28 14:46:55 +0800

    synchronized update

--- third_party/ycmd/cpp/build/ycm/CMakeFiles/ycm_support_libs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ycm_support_libs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ycm_support_libs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- third_party/ycmd/cpp/build/ycm/tests/CMakeFiles/copy_testdata.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_testdata"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_testdata.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
