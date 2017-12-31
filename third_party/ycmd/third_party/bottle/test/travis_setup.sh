53e495f44420abd764172efed5bc8cf4cd20430d
Anthony Sottile <asottile@umich.edu>
2017-12-31 14:35:16 -0800

    Update deadsnakes ppa name
    The deadsnakes ppa has changed names!  For more information, see deadsnakes/issues#54
    
    Committed via https://github.com/asottile/all-repos

--- third_party/ycmd/third_party/bottle/test/travis_setup.sh
+++ third_party/ycmd/third_party/bottle/test/travis_setup.sh
@@ -5,7 +5,7 @@ set -x
 
 if test -n "$PY"; then
   # https://github.com/pycurl/pycurl/blob/master/tests/travis/setup.sh
-  sudo add-apt-repository -y ppa:fkrull/deadsnakes
+  sudo add-apt-repository -y ppa:deadsnakes/ppa
   sudo apt-get update
   sudo apt-get install python$PY-dev
 
